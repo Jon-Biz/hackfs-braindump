1bbbbaabbb398a63b08e4d7176a005cf84ebb0bd
Jonathan El-Bizri <github@elbizri.com>
2021-08-10 22:12:58 -0700

    WIP: revert text editor behaviour

--- demo/src/App/Main/CurrentItems/Item/Text.js
+++ demo/src/App/Main/CurrentItems/Item/Text.js
@@ -1,6 +1,6 @@
-import React from 'react'
+import React, { useState } from 'react'
 import {
-    useRecoilState,
+    useRecoilState
   } from 'recoil';
 
 import {
@@ -16,48 +16,55 @@ import './Text.css'
 
 import itemText from '../../../../Recoil/Atoms/itemText'
 
-const EditorComponent = 
-        () => {
-                    const [ 
-                      text
-                    , setTextState 
-                    ] = useRecoilState(itemText)
+const textToState =
+        text => {
+                  let editorState
 
-                    let editorState
+                  try {
+                    const rawJson = JSON.parse(text)
+                    const contentState = convertFromRaw(rawJson)
+                    editorState = EditorState.createWithContent(contentState)
+                  }
+                  catch (e) {
+                    console.log('fail whale')
+                    editorState = EditorState.createEmpty()
+                    // const raw = convertToRaw(editorState.getCurrentContent())
+                    // const rawStr = JSON.stringify(raw)
+                    // const rawJson = JSON.parse(rawStr)
+                    // const unRaw = convertFromRaw(rawJson)
+                    // editorState = EditorState.createWithContent(unRaw)
+                  }
 
-                    try {
-                      debugger
-                      const rawJson = JSON.parse(text)
-                      const contentState = convertFromRaw(rawJson)
-                      editorState = EditorState.createWithContent(contentState)
-                    }
-                    catch (e) {
-                      console.log('fail whale')
-                      editorState = EditorState.createEmpty()
-                      // const raw = convertToRaw(editorState.getCurrentContent())
-                      // const rawStr = JSON.stringify(raw)
-                      // const rawJson = JSON.parse(rawStr)
-                      // const unRaw = convertFromRaw(rawJson)
-                      // editorState = EditorState.createWithContent(unRaw)
-                    }
-            
-                    const onChange = 
-                            editorState => { 
-                                const json = convertToRaw(editorState.getCurrentContent())
-                                const text = JSON.stringify(json)
-                                setTextState(text) 
-                            }
-
-                    return <Editor 
-                              editorState={editorState} 
-                              onChange={onChange} 
-                            />
+                  return editorState
                 }
+
+const EditorStateComponent = 
+        () => {
+                const [ 
+                  text
+                , setTextState 
+                ] = useRecoilState(itemText)
+
+                const editorState = textToState(text)
+        
+                const onChange = 
+                        editorState => { 
+                            const json = convertToRaw(editorState.getCurrentContent())
+                            const text = JSON.stringify(json)
+
+                            setTextState(text) 
+                        }
+
+                return <Editor 
+                          editorState={editorState} 
+                          onChange={onChange} 
+                        />
+              }
 
 const Text =
         () => (
             <div className="text-editor">
-                <EditorComponent />
+                <EditorStateComponent />
             </div>
         )  
 
